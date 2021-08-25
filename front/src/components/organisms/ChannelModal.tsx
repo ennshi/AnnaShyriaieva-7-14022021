8a9bab4ab4090ce23c97476b3d682a61aaf063e1
Anna Sh <a.shyriaieva@gmail.com>
2021-08-25 22:55:52 +0200

    create new channel

--- front/src/components/organisms/ChannelModal.tsx
+++ front/src/components/organisms/ChannelModal.tsx
@@ -1,16 +1,22 @@
+import React, { useState } from "react";
+import { IoMdAdd, IoMdTrash } from "react-icons/io";
+
 import {
+  Button,
+  HStack,
   Input,
-  Text,
   Modal,
   ModalBody,
-  ModalContent,
-  ModalOverlay,
-  VStack,
   ModalCloseButton,
-  HStack,
+  ModalContent,
+  Text,
+  VStack,
 } from "@chakra-ui/react";
-import React from "react";
+
 import { User } from "../../types";
+import { useCreateChannel } from "../../hooks/mutations/useCreateChannel";
+import { useEffect } from "react";
+import { GET_CHANNELS } from "../../hooks/queries/useGetChannels";
 
 type Props = {
   isOpen: boolean;
@@ -19,22 +25,90 @@ type Props = {
 };
 
 const ChannelModal: React.FC<Props> = ({ isOpen, onClose, users }) => {
+  const [selectedUsers, setSelectedUsers] = useState<string[]>([]);
+  const [channelName, setChannelName] = useState<string>();
+  const [error, setError] = useState<string>();
+
+  const [createChannel, { error: createChannelError }] = useCreateChannel({
+    refetchQueries: [GET_CHANNELS],
+  });
+
+  useEffect(() => {
+    createChannelError && setError(createChannelError?.message);
+  }, [createChannelError]);
+
+  const selectUser = (id: string) =>
+    setSelectedUsers(
+      selectedUsers.includes(id)
+        ? selectedUsers.filter((u) => u !== id)
+        : [...selectedUsers, id]
+    );
+
+  const _onClose = () => {
+    onClose();
+    setChannelName("");
+    setSelectedUsers([]);
+    setError("");
+  };
+
+  const onCreateChannel = async () => {
+    if (!channelName || !selectedUsers.length) return;
+    try {
+      const res = await createChannel({
+        variables: {
+          name: channelName,
+          users: selectedUsers,
+        },
+      });
+      if (res.data && !res.errors) {
+        _onClose();
+      }
+    } catch (e) {}
+  };
+
   return (
-    <Modal isOpen={isOpen} onClose={onClose}>
-      <ModalOverlay bg="brand.primary" />
+    <Modal isOpen={isOpen} onClose={_onClose}>
       <ModalContent>
         <ModalBody px="30px" py="30px">
-          <ModalCloseButton />
-          <VStack>
-            <Text>Create Channel</Text>
-            <Input name="channelName" placeholder="Name" />
-            <VStack>
+          <ModalCloseButton onClick={_onClose} />
+          <VStack spacing="15px" width="100%">
+            <Text fontSize="xl" color="brand.secondary" fontWeight="bold">
+              Create Channel
+            </Text>
+            <Input
+              name="channelName"
+              placeholder="Name"
+              onChange={(e) => setChannelName(e.target.value)}
+              maxWidth="300px"
+            />
+            <VStack spacing="5px" width="100%">
               {users.map((u, i) => (
-                <HStack key={i}>
-                  <Text>{u.username}</Text>
+                <HStack key={i} width="100%" justifyContent="center">
+                  <Button
+                    onClick={() => selectUser(u.id)}
+                    variant="outline"
+                    width="100%"
+                    maxWidth="300px"
+                    justifyContent="space-between"
+                  >
+                    <Text>{u.username}</Text>
+                    {selectedUsers.includes(u.id) ? <IoMdTrash /> : <IoMdAdd />}
+                  </Button>
                 </HStack>
               ))}
             </VStack>
+            {error && (
+              <Text
+                fontSize="xs"
+                color="red.200"
+                width="100%"
+                my="5px"
+                textAlign="center"
+              >
+                {error}
+              </Text>
+            )}
+            <Button onClick={onCreateChannel}>Create</Button>
           </VStack>
         </ModalBody>
       </ModalContent>
--- front/src/components/organisms/ChannelsAndUsers.tsx
+++ front/src/components/organisms/ChannelsAndUsers.tsx
@@ -1,49 +1,82 @@
+import React, { useEffect } from "react";
+import { IoMdAdd } from "react-icons/io";
+
 import {
   Avatar,
   Button,
   HStack,
   IconButton,
   Text,
+  useDisclosure,
   VStack,
 } from "@chakra-ui/react";
-import React, { useEffect } from "react";
+
 import { useCurrentUser } from "../../contexts/currentUserContext";
 import { useCreateChannel } from "../../hooks/mutations/useCreateChannel";
-import { useGetChannels } from "../../hooks/queries/useGetChannels";
+import {
+  GET_CHANNELS,
+  useGetChannels,
+} from "../../hooks/queries/useGetChannels";
 import { useGetUsers } from "../../hooks/queries/useGetUsers";
 import { Channel, User } from "../../types";
-import { IoMdAdd } from "react-icons/io";
+import ChannelModal from "./ChannelModal";
 
 type Props = {
   setChannelId: (id: string) => void;
 };
 
+const isCurrentUserSavedMessagesChannel = (
+  name: string,
+  currentUserId: string
+) => {
+  return name === `saved--${currentUserId}`;
+};
+
+const isDirectChannel = (name: string) => {
+  return name.includes("direct--");
+};
+
+const isDirectChannelWithUser = (
+  name: string,
+  currentUserId: string,
+  userId: string
+) => {
+  return (
+    name === `direct--${currentUserId}--${userId}` ||
+    name === `direct--${userId}--${currentUserId}`
+  );
+};
+
 const ChannelsAndUsers: React.FC<Props> = ({ setChannelId }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   const { data: usersData } = useGetUsers();
   const { data: channelsData } = useGetChannels();
   const { currentUser } = useCurrentUser();
 
-  const [createChannel] = useCreateChannel();
+  const [createChannel] = useCreateChannel({ refetchQueries: [GET_CHANNELS] });
 
   useEffect(() => {
     if (!channelsData?.channels) return;
     const generalChannelId = channelsData?.channels?.find(
-      (ch: Channel) => ch.name === "general"
-    )?.id;
+      (ch: Channel) => ch?.name === "general"
+    );
     setChannelId(generalChannelId);
-  }, [channelsData, setChannelId]);
+  }, [channelsData, currentUser?.id, setChannelId]);
 
   const finalChannels =
     channelsData?.channels?.filter(
-      (ch: Channel) => ch?.users?.length > 2 || ch.name === "general"
+      (ch: Channel) =>
+        !isDirectChannel(ch.name) &&
+        !isCurrentUserSavedMessagesChannel(ch.name, currentUser?.id || "")
     ) || [];
 
   const onChannelClick = (id: string) => setChannelId(id);
   const onUserClick = async (userId: string) => {
     const foundChannel = channelsData?.channels?.find((ch: Channel) =>
       userId === currentUser?.id
-        ? ch?.users?.length === 1
-        : ch?.users?.length === 2 && !!ch?.users?.find((u) => u.id === userId)
+        ? isCurrentUserSavedMessagesChannel(ch?.name, currentUser?.id)
+        : isDirectChannelWithUser(ch?.name, currentUser?.id || "", userId)
     );
     if (foundChannel) return setChannelId(foundChannel.id);
     try {
@@ -62,72 +95,83 @@ const ChannelsAndUsers: React.FC<Props> = ({ setChannelId }) => {
     }
   };
 
-  const createNewChannel = () => {};
+  const createNewChannel = () => onOpen();
 
   return (
-    <VStack
-      width="300px"
-      bgColor="brand.primary"
-      height="100vh"
-      spacing="20px"
-      textAlign="left"
-      overflowY="scroll"
-      px="10px"
-    >
-      <VStack spacing="0" w="100%">
-        <HStack justifyContent="space-between" w="100%">
-          <Text color="gray.100">CHANNELS</Text>
-          {currentUser?.isAdmin && (
-            <IconButton
-              aria-label="send image"
-              bg="transparent"
-              borderRadius="none"
-              width={10}
-              onClick={createNewChannel}
-              icon={<IoMdAdd color="white" />}
-            />
-          )}
-        </HStack>
-        {!!finalChannels?.length &&
-          finalChannels.map((ch: any, i: number) => (
-            <Button
-              variant="unstyled"
-              key={i}
-              color="gray.100"
-              onClick={() => onChannelClick(ch.id)}
-              w="100%"
-              textAlign="left"
-              paddingLeft="10px"
-            >{`#${ch.name}`}</Button>
-          ))}
+    <>
+      <VStack
+        width="300px"
+        bgColor="brand.primary"
+        height="100vh"
+        spacing="20px"
+        textAlign="left"
+        overflowY="scroll"
+        px="10px"
+      >
+        <VStack spacing="0" w="100%">
+          <HStack justifyContent="space-between" w="100%">
+            <Text color="gray.100">CHANNELS</Text>
+            {currentUser?.isAdmin && (
+              <IconButton
+                aria-label="send image"
+                bg="transparent"
+                borderRadius="none"
+                width={10}
+                onClick={createNewChannel}
+                icon={<IoMdAdd color="white" />}
+              />
+            )}
+          </HStack>
+          {!!finalChannels?.length &&
+            finalChannels.map((ch: any, i: number) => (
+              <Button
+                variant="unstyled"
+                key={i}
+                color="gray.100"
+                onClick={() => onChannelClick(ch.id)}
+                w="100%"
+                textAlign="left"
+                paddingLeft="10px"
+              >{`#${ch.name}`}</Button>
+            ))}
+        </VStack>
+        <VStack spacing="0" w="100%">
+          <Text color="gray.100" w="100%">
+            USERS
+          </Text>
+          {!!usersData?.users?.length &&
+            usersData.users.map((u: User, i: number) => (
+              <Button
+                variant="unstyled"
+                key={i}
+                color="gray.100"
+                onClick={() => onUserClick(u.id)}
+                w="100%"
+                textAlign="left"
+                paddingLeft="10px"
+              >
+                <HStack>
+                  <Avatar
+                    name={u.firstName + " " + u.lastName}
+                    size="xs"
+                    bg="#AED6F1"
+                  />
+                  <Text>{`${u.username}`}</Text>
+                </HStack>
+              </Button>
+            ))}
+        </VStack>
       </VStack>
-      <VStack spacing="0" w="100%">
-        <Text color="gray.100" w="100%">
-          USERS
-        </Text>
-        {!!usersData?.users?.length &&
-          usersData.users.map((u: User, i: number) => (
-            <Button
-              variant="unstyled"
-              key={i}
-              color="gray.100"
-              onClick={() => onUserClick(u.id)}
-              w="100%"
-              textAlign="left"
-              paddingLeft="10px"
-            >
-              <HStack>
-                <Avatar
-                  name={u.firstName + " " + u.lastName}
-                  size="xs"
-                  bg="#AED6F1"
-                />
-                <Text>{`${u.username}`}</Text>
-              </HStack>
-            </Button>
-          ))}
-      </VStack>
-    </VStack>
+      <ChannelModal
+        isOpen={isOpen}
+        onClose={onClose}
+        users={
+          (usersData &&
+            usersData.users.filter((u: User) => u.id !== currentUser?.id)) ||
+          []
+        }
+      />
+    </>
   );
 };
 
